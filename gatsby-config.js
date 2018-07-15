d55818448283609e5b64fed9e960e82c5fe2a17f
barnettjoe <joebarnett876@gmail.com>
2018-07-15 13:33:10 +0100

    basic

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,23 @@
 module.exports = {
-  plugins: [`gatsby-plugin-netlify-cms`],
+  siteMetadata: {
+    title: `Cole & Lopez`,
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
+      },
+    },
+    `gatsby-transformer-remark`,
+    `gatsby-plugin-netlify-cms`,
+    `gatsby-plugin-glamor`,
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+  ],
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,43 @@
 import React from "react";
-import './reset.css';
-import Parallax from "../components/Parallax/Parallax.js";
-import ParallaxBaseLayer from "../components/ParallaxBaseLayer/ParallaxBaseLayer.js";
-import ParallaxBackLayer from "../components/ParallaxBackLayer/ParallaxBackLayer.js";
+import g from "glamorous";
 
-export default function () {
-	return (
-		<Parallax>
-			<ParallaxBaseLayer>
-				<p>Lorem ipsum dolor amet celiac health goth food truck, photo booth meggings cliche shaman. Fashion axe fam franzen VHS hexagon ugh. Trust fund lumbersexual flexitarian post-ironic neutra occupy. Keytar vinyl tilde, hell of portland vape whatever quinoa cliche. Gentrify wolf hot chicken shabby chic snackwave. Poutine fingerstache single-origin coffee cardigan street art.</p>
-			</ParallaxBaseLayer>
-			<ParallaxBackLayer>
-				<p>Marfa green juice tbh mixtape dreamcatcher. Art party gochujang man braid, tumblr health goth edison bulb offal man bun kogi. Single-origin coffee hot chicken hell of cold-pressed adaptogen, tacos forage 3 wolf moon hammock craft beer meh heirloom. Hell of truffaut chambray blue bottle snackwave literally banjo put a bird on it farm-to-table man bun. Art party live-edge before they sold out hella mixtape helvetica. Kickstarter hot chicken pop-up tote bag enamel pin, letterpress tumblr banjo taiyaki bicycle rights knausgaard.</p>
-			</ParallaxBackLayer>
-		</Parallax>
-	);
-}
+import { rhythm } from "../utils/typography";
+
+export default ({ data }) => {
+  console.log(data);
+  return (
+    <div>
+      <g.H1 display={"inline-block"} borderBottom={"1px solid"}>
+        Amazing Pandas Eating Things
+      </g.H1>
+      <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
+      {data.allMarkdownRemark.edges.map(({ node }) => (
+        <div key={node.id}>
+          <g.H3 marginBottom={rhythm(1 / 4)}>
+            {node.frontmatter.title}{" "}
+            <g.Span color="#BBB">— {node.frontmatter.date}</g.Span>
+          </g.H3>
+          <p>{node.excerpt}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export const query = graphql`
+  query IndexQuery {
+    allMarkdownRemark {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "DD MMMM, YYYY")
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`;
